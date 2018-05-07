700afa92b39ee5adbd9daca2e6b12cd289a1b08d
Todd <todd.r.lasley@gmail.com>
2018-05-06 20:03:44 -0400

    initial commit from assignment provided Percolation object structure

---
+++ percolation/Percolation.java
@@ -0,0 +1,39 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+   
+    public Percolation(int n) {
+
+    }
+
+    // open site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+    }
+    
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col){
+        return true;
+    }
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col){
+        return true;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites(){
+        return 0;
+    }
+    
+    // does the system percolate?
+    public boolean percolates(){
+        return false;
+    }
+
+    // test client (optional)
+    public static void main(String[] args){
+    }
+}
