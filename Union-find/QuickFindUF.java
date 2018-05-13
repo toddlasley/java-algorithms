340899e5c48126ab3898c9479fa0a6c0f5be2528
Todd <todd.r.lasley@gmail.com>
2018-05-13 17:59:40 -0400

    adding quick-find algorithm for union-find problem

---
+++ Union-find/QuickFindUF.java
@@ -0,0 +1,35 @@
+//Quick-find approach to dynamic connectivity approach
+//eager approach
+
+public class QuickFindUF
+{
+    private int[] id;
+    
+    public QuickFindUF(int N)
+    {
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+    
+    //check whether p and q are in the same component
+    //2 array accesses
+    public boolean connected(int p, int q)
+    {
+        return id[p] == id[q];
+    }
+    
+    //change all entries with id[p] to id[q]
+    //at most 2N + 2 array accesses
+    public void union(int p, int q)
+    {
+        int pid = id[p];
+        int qid = id[q];
+        
+        for (int i = 0; i < id.length; i++)
+        {
+            if(id[i] == pid)
+                id[i] = qid;
+        }
+    }
+}
