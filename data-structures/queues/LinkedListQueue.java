102b40f4470fe69ed38f12b9c4a608d828a2a170
Todd <todd.r.lasley@gmail.com>
2018-06-11 23:41:20 -0400

    adding generic Queue with underlying linked list structure

---
+++ data-structures/queues/LinkedListQueue.java
@@ -0,0 +1,45 @@
+public class LinkedListQueue<T>
+{
+    private Node first, last;
+    
+    private class Node
+    {
+        T item;
+        Node next;
+    }
+    
+    public boolean isEmpty()
+    {
+        return first == null;
+    }
+    
+    public void enqueue(T item)
+    {
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        
+        if(this.isEmpty())
+        {
+            first = last;
+        }
+        else
+        {
+            oldLast.next = last;
+        }
+    }
+    
+    public T dequeue()
+    {
+        T item = first.item;
+        first = first.next;
+        
+        if(this.isEmpty())
+        {
+            last = null;
+        }
+        
+        return item;
+    }
+}
