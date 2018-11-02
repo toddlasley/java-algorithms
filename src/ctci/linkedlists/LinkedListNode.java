e27863020ae431cea22c8ede9f87010f2b893287
Todd <todd.r.lasley@gmail.com>
2018-11-01 23:34:33 -0400

    - added Sum Lists problem from CTCI - added PartialSum class for Sum Lists problem - LinkedListNode has setNext() method for adding a node to the end of the list and a length() method for getting the length

--- src/ctci/linkedlists/LinkedListNode.java
+++ src/ctci/linkedlists/LinkedListNode.java
@@ -4,10 +4,36 @@ public class LinkedListNode {
     int data;
     LinkedListNode next;
 
+    public LinkedListNode(){ }
+
     public LinkedListNode(int data, LinkedListNode node){
         this.data = data;
         this.next = node;
     }
+
+    public void setNext(LinkedListNode next){
+        if(this.next == null){
+            this.next = next;
+        } else {
+            LinkedListNode node = this.next;
+            while(node.next != null){
+                node = node.next;
+            }
+            node.next = next;
+        }
+    }
+
+    public int length(){
+        int count = 1;
+        LinkedListNode node = this.next;
+        while(node != null){
+            count++;
+            node = node.next;
+        }
+
+        return count;
+    }
+
 
     @Override()
     public String toString(){
---
+++ src/ctci/linkedlists/PartialSum.java
@@ -0,0 +1,6 @@
+package ctci.linkedlists;
+
+public class PartialSum {
+    public LinkedListNode sum = null;
+    public int carry = 0;
+}
