1a9f2ab1fd809f64c9724518d1ed7016ffe79453
Todd <todd.r.lasley@gmail.com>
2019-12-20 14:55:39 -0500

    adding solution and tests for Stack Min CTCI problem

---
+++ src/main/xyz/toddlasley/ctci/stacksandqueues/StackWithMin.java
@@ -0,0 +1,40 @@
+// 3.2: How would you design a stack which, in addition to 'push' and 'pop', has a function
+//      'min' which returns the minimum element? 'Push', 'pop', and 'min' must operate in O(1)
+//      time.
+//
+// Question doesn't specify, but given the book's implementation, I assume we are only dealing
+// with unique values
+
+package xyz.toddlasley.ctci.stacksandqueues;
+
+import java.util.Stack;
+
+public class StackWithMin extends Stack<Integer> {
+    private Stack<Integer> minStack;
+
+    public StackWithMin() {
+        this.minStack = new Stack<Integer>();
+    }
+
+    public void push (int value) {
+        if ( value < this.min() ) {
+            this.minStack.push(value);
+        }
+
+        super.push(value);
+    }
+
+    public Integer pop () {
+        int value = super.pop();
+
+        if ( value == min() ) {
+            minStack.pop();
+        }
+
+        return value;
+    }
+
+    public int min () {
+        return this.minStack.isEmpty() ? Integer.MAX_VALUE : this.minStack.peek();
+    }
+}
---
+++ src/test/xyz/toddlasley/ctci/stacksandqueues/StackWithMinTest.java
@@ -0,0 +1,51 @@
+package xyz.toddlasley.ctci.stacksandqueues;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackWithMinTest {
+    private StackWithMin stack;
+
+    @Before
+    public void initializeStack () {
+        this.stack = new StackWithMin();
+        this.stack.push(5);
+        this.stack.push(3);
+        this.stack.push(6);
+        this.stack.push(7);
+        this.stack.push(8);
+    }
+
+    @Test
+    public void pushAddsValueToStackAndSetsNewMinAccordingly () {
+        assertEquals(3, this.stack.min());
+        this.stack.push(2);
+        this.stack.push(10);
+        assertEquals(10, (int)this.stack.peek());
+        assertEquals(2, this.stack.min());
+    }
+
+    @Test
+    public void popRemovesValueFromStackAndSetsNewMinAccordingly () {
+        assertEquals(8, (int)this.stack.pop());
+        assertEquals(3, this.stack.min());
+
+        assertEquals(7, (int)this.stack.pop());
+        assertEquals(3, this.stack.min());
+
+        assertEquals(6, (int)this.stack.pop());
+        assertEquals(3, this.stack.min());
+
+        assertEquals(3, (int)this.stack.pop());
+        assertEquals(5, this.stack.min());
+    }
+
+    @Test
+    public void minReturnsIntegerMaxValueIfMinStackIsEmpty () {
+        this.stack = new StackWithMin();
+
+        assertEquals(Integer.MAX_VALUE, this.stack.min());
+    }
+}
