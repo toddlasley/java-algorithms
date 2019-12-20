635a0c78bb00aba2b429c29561a4390df372dd94
Todd <todd.r.lasley@gmail.com>
2019-12-20 14:03:53 -0500

    adding solution and tests for Three in One stack CTCI problem

---
+++ src/main/xyz/toddlasley/ctci/stacksandqueues/FixedMultiStack.java
@@ -0,0 +1,61 @@
+// 3.1: Describe how you could use a single array to implement three stacks.
+// Solution: p. 227
+
+package xyz.toddlasley.ctci.stacksandqueues;
+
+public class FixedMultiStack {
+    private int numberOfStacks = 3;
+    private int stackCapacity;
+    private int[] values;
+    private int[] sizes;
+
+    public FixedMultiStack (int stackSize) {
+        this.stackCapacity = stackSize;
+        this.values = new int[ stackSize * this.numberOfStacks];
+        this.sizes = new int[stackSize];
+    }
+
+    public void push (int stackIndex, int value) throws Exception {
+        if ( this.isFull(stackIndex) ) {
+            throw new Exception(String.format("Stack %d is full", stackIndex));
+        }
+
+        this.sizes[stackIndex]++;
+        this.values[this.indexOfTop(stackIndex)] = value;
+    }
+
+    public int pop (int stackIndex) throws Exception {
+        if ( this.isEmpty(stackIndex) ) {
+            throw new Exception(String.format("Stack %d is empty"));
+        }
+
+        int indexOfTop = this.indexOfTop(stackIndex);
+        int value = this.values[indexOfTop];
+        this.values[indexOfTop] = 0;
+        this.sizes[stackIndex]--;
+
+        return value;
+    }
+
+    public int peek (int stackIndex) throws Exception {
+        if ( this.isEmpty(stackIndex)) {
+            throw new Exception(String.format("Stack %d is empty"));
+        }
+
+        return this.values[this.indexOfTop(stackIndex)];
+    }
+
+    public boolean isEmpty (int stackIndex) {
+        return this.sizes[stackIndex] == 0;
+    }
+
+    public boolean isFull (int stackIndex) {
+        return this.sizes[stackIndex] == this.stackCapacity;
+    }
+
+    public int indexOfTop(int stackIndex) {
+        int offset = stackIndex * this.stackCapacity;
+
+        return offset + this.sizes[stackIndex] - 1;
+    }
+}
---
+++ src/test/xyz/toddlasley/ctci/stacksandqueues/FixedMultiStackTest.java
@@ -0,0 +1,83 @@
+package xyz.toddlasley.ctci.stacksandqueues;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class FixedMultiStackTest {
+    private FixedMultiStack multiStack;
+
+    @Before
+    public void intializeStack() throws Exception {
+        this.multiStack = new FixedMultiStack(4);
+        this.multiStack.push(0, 1);
+        this.multiStack.push(0, 2);
+        this.multiStack.push(1, 3);
+        this.multiStack.push(1, 4);
+        this.multiStack.push(1, 5);
+        this.multiStack.push(1, 6);
+    }
+
+
+    @Test
+    public void isFullIndicatesIfGivenStackIsFull() {
+        assertFalse(this.multiStack.isFull(0));
+        assertTrue(this.multiStack.isFull(1));
+        assertFalse(this.multiStack.isFull(2));
+    }
+
+    @Test
+    public void indexOfTopReturnsTopIndexOfGivenStack() {
+        assertEquals(1, this.multiStack.indexOfTop(0));
+        assertEquals(7, this.multiStack.indexOfTop(1));
+        assertEquals(7, this.multiStack.indexOfTop(2));
+
+    }
+
+    @Test
+    public void isFullIndicatesIfGivenStackIsEmpty() {
+        assertFalse(this.multiStack.isEmpty(0));
+        assertFalse(this.multiStack.isEmpty(1));
+        assertTrue(this.multiStack.isEmpty(2));
+    }
+
+    @Test
+    public void popPushAndPeekAllInteractWithTheAppropriateStack() throws Exception {
+        // stack 0 tests
+        assertEquals(2, this.multiStack.pop(0));
+        assertEquals(1, this.multiStack.peek(0));
+
+        this.multiStack.push(0, 3);
+        assertEquals(3, this.multiStack.peek(0));
+
+        // stack 1 tests
+        assertEquals(6, this.multiStack.pop(1));
+        assertEquals(5, this.multiStack.peek(1));
+
+        this.multiStack.push(1, 7);
+        assertEquals(7, this.multiStack.peek(1));
+
+        // stack 2 tests
+        this.multiStack.push(2, 10);
+        this.multiStack.push(2, 11);
+        assertEquals(11, this.multiStack.peek(2));
+        assertEquals(11, this.multiStack.pop(2));
+        assertEquals(10, this.multiStack.peek(2));
+    }
+
+    @Test ( expected = Exception.class)
+    public void popThrowsExceptionIfStackIsEmpty() throws Exception {
+        this.multiStack.pop(2);
+    }
+
+    @Test ( expected = Exception.class)
+    public void pushThrowsExceptionIfStackIsFull() throws Exception {
+        this.multiStack.push(1, 10);
+    }
+
+    @Test ( expected = Exception.class)
+    public void peekThrowsExceptionIfStackIsEmpty() throws Exception {
+        this.multiStack.peek(2);
+    }
+}
