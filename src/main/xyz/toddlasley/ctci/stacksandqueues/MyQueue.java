32ef176d2dee4d00ea7922c143f4a991ee437c76
Todd <todd.r.lasley@gmail.com>
2019-12-22 15:00:42 -0500

    adding solution and test for CTCI problem involving a queue implemented as two stacks

---
+++ src/main/xyz/toddlasley/ctci/stacksandqueues/MyQueue.java
@@ -0,0 +1,42 @@
+// 3.4: Implement a MyQueue class which implements a queue using two stacks.
+// Solution: p. 236
+
+package xyz.toddlasley.ctci.stacksandqueues;
+
+import java.util.Stack;
+
+public class MyQueue<T> {
+    private Stack<T> newest;
+    private Stack<T> oldest;
+
+    public MyQueue () {
+        this.newest = new Stack<T>();
+        this.oldest = new Stack<T>();
+    }
+
+    public int size () {
+        return this.newest.size() + this.oldest.size();
+    }
+
+    public void enqueue (T value) {
+        this.newest.push(value);
+    }
+
+    public T dequeue () {
+        this.shiftStacks();
+        return this.oldest.pop();
+    }
+
+    public T peek () {
+        this.shiftStacks();
+        return this.oldest.peek();
+    }
+
+    private void shiftStacks () {
+        if ( this.oldest.isEmpty() ) {
+            while ( !this.newest.isEmpty() ) {
+                this.oldest.push(this.newest.pop());
+            }
+        }
+    }
+}
