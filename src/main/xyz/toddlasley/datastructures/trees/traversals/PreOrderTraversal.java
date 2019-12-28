9d7b47eb599803fcdd03a74540cbb2e240a96d79
Todd <todd.r.lasley@gmail.com>
2019-12-28 14:59:42 -0500

    adding pre-order binary tree traversal solution and tests

---
+++ src/main/xyz/toddlasley/datastructures/trees/traversals/PreOrderTraversal.java
@@ -0,0 +1,29 @@
+package xyz.toddlasley.datastructures.trees.traversals;
+
+import xyz.toddlasley.datastructures.trees.TreeNode;
+
+public class PreOrderTraversal {
+    public static String traverse(TreeNode root) {
+        if (root != null) {
+            StringBuilder sb = new StringBuilder();
+
+            traverseHelper(root, sb);
+
+            return sb.toString();
+        } else {
+            return null;
+        }
+    }
+
+    private static void traverseHelper(TreeNode node, StringBuilder sb) {
+        sb.append(String.format("(%d)", node.data));
+
+        if ( node.left != null ) {
+            traverseHelper(node.left, sb);
+        }
+
+        if ( node.right != null ) {
+            traverseHelper(node.right, sb);
+        }
+    }
+}
