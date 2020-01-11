2245762e0b2cf735c4ce82932eeac32830db7b42
Todd <todd.r.lasley@gmail.com>
2020-01-11 16:23:39 -0600

    adding solution and test for Most Common Word LeetCode problem

---
+++ src/test/xyz/toddlasley/leetcode/easy/arraysandstrings/MostCommonWordTest.java
@@ -0,0 +1,18 @@
+package xyz.toddlasley.leetcode.easy.arraysandstrings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MostCommonWordTest {
+
+    @Test
+    public void mostCommonWordFindsMostCommonWordThatIsntBanned() {
+        String paragraph = "This is a new string. String is good. More STRING";
+        String[] banned = {"string"};
+        assertEquals("is", MostCommonWord.mostCommonWord(paragraph, banned));
+
+        String[] banned1 = {};
+        assertEquals("string", MostCommonWord.mostCommonWord(paragraph, banned1));
+    }
+}
