618936ef93041c280a8b55f027f5a13db1c18fdd
toddlasley <todd.r.lasley@gmail.com>
2024-02-06 19:38:31 -0800

    reverse to make equal problem???

---
+++ src/test/xyz/toddlasley/interviews/arraysandstrings/ReverseToMakeEqualTest.java
@@ -0,0 +1,15 @@
+package xyz.toddlasley.interviews.arraysandstrings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ReverseToMakeEqualTest {
+    @Test
+    public void shouldDetermineIfArraysCanBeReversedToEqual() {
+        assertTrue(ReverseToMakeEqual.areEqual(new int[]{1, 2, 3, 4}, new int[]{1, 4, 3, 2}));
+        assertTrue(ReverseToMakeEqual.areEqual(new int[]{1, 2, 3, 4, 5}, new int[]{1, 5, 4, 3, 2}));
+        assertFalse(ReverseToMakeEqual.areEqual(new int[]{1, 2, 3, 4}, new int[]{1, 4, 3, 3}));
+    }
+}
